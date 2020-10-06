e5b9bb6c2b2b58e34f59bde8e47a8cdc5b874e29
BARRY <BARRY@192.168.192.31>
2020-10-06 19:05:15 +0100

    Add Java Swing Project

---
+++ tutuka18/src/tutuka18/tutuka18.java
@@ -0,0 +1,13 @@
+package tutuka18;
+
+
+public class tutuka18 {
+
+	// entry point to program
+	public static void main(String[] args) {
+
+		MyJFrame frame = null;
+		frame = new MyJFrame();
+	}
+
+}
