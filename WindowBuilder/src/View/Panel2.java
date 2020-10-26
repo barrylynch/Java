8b72a55192ae8c23b8354a16fb40056b4ce5f057
barrylynch <barrylinch@gmail.com>
2020-10-26 15:48:33 +0000

    initial msg from commandline

---
+++ WindowBuilder/src/View/Panel2.java
@@ -0,0 +1,13 @@
+package View;
+
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
+public class Panel2 extends JPanel {
+
+
+	Panel2(){
+		setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+	}
+
+}
---
+++ WindowBuilder/src/View/Panel3.java
@@ -0,0 +1,11 @@
+package View;
+
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
+public class Panel3 extends JPanel{
+
+	Panel3(){
+		setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+	}
+}
